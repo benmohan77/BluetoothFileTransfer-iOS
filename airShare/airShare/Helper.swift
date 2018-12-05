a6e7b11ef1723f6cd53a6926d2ebce0c8ab6fe7a
Tyler Gaffaney <tylergaffaney97@gmail.com>
2018-12-05 14:40:35 -0600

    Big fix

--- airShare/airShare/Helper.swift
+++ airShare/airShare/Helper.swift
@@ -77,36 +77,83 @@ class Helper {
     static func alert(data: Data, vc: UIViewController){
         if let image = UIImage.init(data: data){
             let alert = UIAlertController(title: "File Received!", message: "", preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-            
-            let imageWidth = image.size.width * 3
-            let imageHeight = image.size.height * 3
-            let alertWidth = alert.view.frame.size.width - 20.0
-            
-            let scale = alertWidth / imageWidth
-            
-            let newHeight = scale * imageHeight
-            let newWidth = scale * imageWidth
-            let imgViewTitle = UIImageView(frame: CGRect(x: 0, y: 55, width: newWidth, height: newHeight))
-            let edges = UIEdgeInsets(top: 0, left: 0, bottom: newHeight, right: newWidth)
-            let newImage = image.resizableImage(withCapInsets: edges)
-            imgViewTitle.image = newImage
-            
-            alert.view.addSubview(imgViewTitle)
+            let action = UIAlertAction(title: "Accept", style: .default) { (alert) in
+                // image to share
+                // set up activity view controller
+                let imageToShare: [UIImage] = [image]
+                let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+                activityViewController.popoverPresentationController?.sourceView = vc.view // so that iPads won't crash
+                
+                // exclude some activity types from the list (optional)
+                activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+                
+                // present the view controller
+                vc.present(activityViewController, animated: true, completion: nil)
+            }
+            let ac = UIAlertAction(title: "Discard", style: .destructive) { (asfd) in
+                
+            }
+            alert.addAction(ac)
             alert.addAction(action)
             
-            let desiredHeight: CGFloat = newHeight + 100
-            var height:NSLayoutConstraint = NSLayoutConstraint(item: alert.view, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: desiredHeight)
-            alert.view.addConstraint(height);
-            print(alert.view.frame.width)
             
-            vc.present(alert, animated: true, completion: nil)
-//            vc.present(alert, animated: true) {
-//                <#code#>
-//            }
+            print(alert.view.frame.width)
+            DispatchQueue.main.async {
+                alert.view.alpha = 0
+                vc.present(alert, animated: true) {
+                    let imageWidth = image.size.width
+                    let imageHeight = image.size.height
+                    let alertWidth = alert.view.frame.size.width
+                    
+                    let scale:CGFloat = alertWidth / imageWidth
+                    
+                    let newHeight = scale * imageHeight
+                    let newWidth = scale * imageWidth
+                    let imgViewTitle = UIImageView(frame: CGRect(x: 0, y: 55, width: newWidth, height: newHeight))
+                    
+                    imgViewTitle.image = resizeImage(image: image, targetSize: CGSize(width: newWidth, height: newHeight))
+                    
+                    alert.view.addSubview(imgViewTitle)
+                    
+                    
+                    let desiredHeight: CGFloat = newHeight + 100
+                    var height:NSLayoutConstraint = NSLayoutConstraint(item: alert.view, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: desiredHeight)
+                    alert.view.addConstraint(height);
+                    UIView.animate(withDuration: 0.4, animations: {
+                        alert.view.alpha = 1
+                    })
+                }
+            }
+            
         }else{
             //Data not an image
         }
+    }
+    
+    static func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
     }
 }
 
